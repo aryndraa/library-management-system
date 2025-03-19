c06e01e971c48b0f10c04929bd59fea51b7383e1
aryndraa <aryacoder1102@gmail.com>
2025-03-20 01:36:28 +0800

    room booking model & migration

---
+++ app/Models/RoomBooking.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class RoomBooking extends Model
+{
+    protected $fillable = [
+        'booking_date',
+        'started_time',
+        'finished_time',
+        'status'
+    ];
+
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
