7a87aefe3b9672d321d4caa33511127eaff01f05
aryndraa <aryacoder1102@gmail.com>
2025-03-27 20:27:47 +0800

    member dummy data

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::factory()->count(50)->make();
+
+        User::query()->insert($users->toArray());
+    }
+}
