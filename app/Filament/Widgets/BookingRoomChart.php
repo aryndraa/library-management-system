32b6767d357b6f7eabd09ace5bb3b242e8e3ed5e
aryndraa <aryacoder1102@gmail.com>
2025-03-31 02:00:36 +0800

    Chart booking room filter

--- app/Filament/Widgets/BookingRoomChart.php
+++ app/Filament/Widgets/BookingRoomChart.php
@@ -12,27 +12,74 @@ class BookingRoomChart extends ChartWidget
     protected static ?string $heading = 'All Booking Room Record';
 
     protected static ?string $pollingInterval = '10s';
+
     protected static ?int $sort = 1;
 
     protected static string $color = "primary";
 
+    public ?string $filter = 'year';
 
+
+    protected function getFilters(): ?array
+    {
+        return [
+            'week' => 'Last week',
+            'month' => 'Last month',
+            'year' => 'This year',
+        ];
+    }
 
     protected function getData(): array
     {
-        $data = Trend::model(RoomBooking::class)
-            ->dateColumn("booking_date")
-            ->between(
-                start: now()->startOfYear(),
-                end: now()->endOfYear()
-            )
-            ->perMonth()
-            ->count();
+        $activeFilter = $this->filter;
+
+        if ($activeFilter == 'year') {
+            $data = Trend::model(RoomBooking::class)
+                ->dateColumn("booking_date")
+                ->between(
+                    start: now()->startOfYear(),
+                    end: now()->endOfYear()
+                )
+                ->perMonth()
+                ->count();
+
+            $labels = $data->map(fn ($item) => Carbon::parse($item->date)->translatedFormat('M'))->toArray();
+        } elseif ($activeFilter == 'month') {
+            $data = Trend::model(RoomBooking::class)
+                ->dateColumn("booking_date")
+                ->between(
+                    start: now()->startOfMonth(),
+                    end: now()->endOfMonth()
+                )
+                ->perWeek()
+                ->count();
+
+            $labels = $data->map(function ($item) {
+                [$year, $week] = explode('-', $item->date); // Pecah jadi tahun & minggu
+                return Carbon::now()->setISODate($year, $week, 1)->translatedFormat('d M');
+            })->toArray();
+        } elseif ($activeFilter == 'week') {
+            $data = Trend::model(RoomBooking::class)
+                ->dateColumn("booking_date")
+                ->between(
+                    start: now()->subDays(6)->startOfDay(),
+                    end: now()->endOfDay()
+                )
+                ->perDay()
+                ->count();
+
+            $labels = $data->map(fn ($item) => Carbon::parse($item->date)->translatedFormat('d M'))->toArray();
+        } else {
+            return [
+                'datasets' => [],
+                'labels' => [],
+            ];
+        }
 
         return [
             'datasets' => [
                 [
-                    'label' => 'Blog posts created',
+                    'label' => 'Room Bookings',
                     'data' => $data->map(fn ($item) => $item->aggregate)->toArray(),
                     'tension' => 0.5,
                     'backgroundColor' => 'rgba(112, 79, 230, 0.05)',
@@ -40,7 +87,7 @@ class BookingRoomChart extends ChartWidget
                     'borderColor' => 'rgba(112, 79, 230, 1)',
                 ],
             ],
-            'labels' => $data->map(fn ($item) => Carbon::parse($item->date)->translatedFormat('M'))->toArray(),
+            'labels' => $labels,
         ];
     }
 
