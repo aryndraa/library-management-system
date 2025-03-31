58afd317c87b878382d23ee6153e994322ed7a03
aryndraa <aryacoder1102@gmail.com>
2025-04-01 01:59:44 +0800

    filter book

--- app/Filament/Resources/LibraryResource/RelationManagers/BooksRelationManager.php
+++ app/Filament/Resources/LibraryResource/RelationManagers/BooksRelationManager.php
@@ -7,6 +7,7 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -33,15 +34,22 @@ class BooksRelationManager extends RelationManager
                 TextColumn::make('isbn')
                     ->sortable(),
                 TextColumn::make('title')
+                    ->searchable()
                     ->sortable(),
-                TextColumn::make('author'),
-                TextColumn::make('publisher'),
+                TextColumn::make('author')
+                    ->searchable(),
+                TextColumn::make('publisher')
+                    ->searchable(),
                 TextColumn::make('category.name'),
                 TextColumn::make('publication_date')
                     ->sortable(),
             ])
             ->filters([
-                //
+                SelectFilter::make('category')
+                    ->relationship('category', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->attribute('category.name'),
             ])
             ->headerActions([
                 Tables\Actions\CreateAction::make(),
