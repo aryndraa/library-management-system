9cdcd788653244b8b592e26d9021b547af9b2a85
aryndraa <aryacoder1102@gmail.com>
2025-04-08 15:19:57 +0800

    room filter

--- app/Filament/Librarian/Resources/RoomResource.php
+++ app/Filament/Librarian/Resources/RoomResource.php
@@ -5,12 +5,18 @@ namespace App\Filament\Librarian\Resources;
 use App\Filament\Librarian\Resources\RoomResource\Pages;
 use App\Filament\Librarian\Resources\RoomResource\RelationManagers;
 use App\Models\Room;
+use Filament\Facades\Filament;
 use Filament\Forms;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Support\RawJs;
 use Filament\Tables;
 use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -27,13 +33,117 @@ class RoomResource extends Resource
     {
         return $form
             ->schema([
-                //
-            ]);
+                Forms\Components\Group::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->required(),
+
+                        Forms\Components\Select::make('room_category_id')
+                            ->relationship('category', 'name')
+                            ->searchable()
+                            ->preload(),
+
+                        Forms\Components\Select::make('library_id')
+                            ->relationship('library', 'name')
+                            ->searchable()
+                            ->default(fn() => Filament::auth()->user()->library_id)
+                            ->preload()
+                            ->disabled()
+                            ->dehydrated(),
+
+                        TextInput::make('price')
+                            ->label('Price')
+                            ->mask(RawJs::make(<<<'JS'
+                                    text => {
+                                        let number = text.replace(/[^\d]/g, '');
+                                        return new Intl.NumberFormat('id-ID', {
+                                            style: 'currency',
+                                            currency: 'IDR',
+                                            minimumFractionDigits: 0
+                                        }).format(number);
+                                    }
+                                JS))
+                            ->dehydrateStateUsing(fn ($state) => (int) preg_replace('/[^\d]/', '', $state))
+                            ->formatStateUsing(fn ($state) => $state ? 'Rp ' . number_format($state, 0, ',', '.') : null) ,
+
+                        Forms\Components\Repeater::make('facilities')
+                            ->relationship('facilities')
+                            ->schema([
+                                TextInput::make('facility')
+                                    ->label('Name'),
+
+                                Forms\Components\Textarea::make('description')
+                                    ->label('Description'),
+
+                            ])
+                            ->grid(2)
+                            ->columnSpan(2)
+                    ])
+                    ->columnSpan(2)
+                    ->columns(2),
+
+                Forms\Components\Group::make()
+                    ->schema([
+                        SpatieMediaLibraryFileUpload::make('picture')
+                            ->collection('room')
+                            ->columnSpan(2),
+
+                        Forms\Components\Section::make()
+                            ->schema([
+                                Forms\Components\Select::make('status')
+                                    ->label('Status')
+                                    ->options([
+                                        'available' => 'Available',
+                                        'booked' => 'Booked',
+                                        'maintenance' => 'Maintenance',
+                                    ])
+                                    ->default('available')
+                                    ->hint(function ($state) {
+                                        return match ($state) {
+                                            'available' => 'Available',
+                                            'booked' => 'Booked',
+                                            'maintenance' => 'Maintenance',
+                                            default => 'Unknown',
+                                        };
+                                    })
+                                    ->hintColor(function ($state) {
+                                        return match ($state) {
+                                            'available' => 'success',
+                                            'booked' => 'warning',
+                                            'maintenance' => 'danger',
+                                            default => 'gray',
+                                        };
+                                    }),
+
+                                Forms\Components\Placeholder::make('total_bookings')
+                                    ->label('Total Bookings')
+                                    ->content(fn ($record) => $record?->bookings()->count() ?? 0)
+                                    ->disabled(),
+
+                                Placeholder::make('total_income')
+                                    ->label('Total Income')
+                                    ->content(function ($record) {
+                                        if (!$record) return 'Rp 0';
+
+                                        $totalBooking = $record->bookings()->count();
+                                        $price = $record->price ?? 0;
+                                        $totalIncome = $totalBooking * $price;
+
+                                        return 'Rp ' . number_format($totalIncome, 0, ',', '.');
+                                    })
+                                    ->disabled(),
+                            ])
+                    ])->columnSpan(['lg' => 1])
+            ])
+            ->columns(3);
     }
 
     public static function table(Table $table): Table
     {
         return $table
+            ->query(
+                Room::query()->where('library_id', Filament::auth()->user()->library_id)
+            )
             ->columns([
                 SpatieMediaLibraryImageColumn::make('room')
                     ->label('Picture')
@@ -67,7 +177,18 @@ class RoomResource extends Resource
 
             ])
             ->filters([
-                //
+                SelectFilter::make('status')
+                    ->options([
+                        'available' => 'Available',
+                        'booked' => 'Booked',
+                        'maintenance' => 'Maintenance',
+                    ]),
+
+                SelectFilter::make('category')
+                    ->relationship('category', 'name')
+                    ->label('Category')
+                    ->preload()
+                    ->searchable(),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -17,6 +17,7 @@ class Room extends Model implements HasMedia
     protected $fillable = [
         'name',
         'room_category_id',
+        'library_id',
         'price',
         'status'
     ];
