72cff1aae58280b3627a6454f98731867e7185ad
aryndraa <aryacoder1102@gmail.com>
2025-04-24 04:33:41 +0800

    reset password style

--- resources/views/filament/clusters/account-settings/pages/password-settings.blade.php
+++ resources/views/filament/clusters/account-settings/pages/password-settings.blade.php
@@ -2,11 +2,14 @@
     <x-filament-panels::form wire:submit="update">
 
 
-        {{ $this->form }}
+        <h2 class="text-3xl font-medium mb-3">Reset Password</h2>
+        <div class="p-4 bg-white flex flex-col gap-6">
+            {{ $this->form }}
 
-        <x-filament-panels::form.actions
-            :actions="$this->getFormActions()"
-        />
+            <x-filament-panels::form.actions
+                :actions="$this->getFormActions()"
+            />
+        </div>
 
 
     </x-filament-panels::form>
