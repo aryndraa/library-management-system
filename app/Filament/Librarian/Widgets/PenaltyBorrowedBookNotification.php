73a6b6ced234af88b7ca2273d1c923af69260a4b
aryndraa <aryacoder1102@gmail.com>
2025-04-25 02:43:07 +0800

    borrowed penalty

---
+++ app/Filament/Librarian/Widgets/PenaltyBorrowedBookNotification.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Filament\Librarian\Widgets;
+
+use App\Models\BorrowedBook;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class PenaltyBorrowedBookNotification extends BaseWidget
+{
+    protected int | string | array $columnSpan = 2;
+
+    protected static ?int $sort = 3;
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                BorrowedBook::query()
+                    ->where('library_id', auth()->user()->library_id)
+                    ->where('status', 'penalty')
+                    ->orderByDesc('created_at')
+                    ->limit(5)
+            )
+            ->columns([
+                TextColumn::make('member.first_name')
+                    ->getStateUsing(fn ($record) =>
+                        $record->member->profile->first_name .
+                        ' '
+                        . $record->member->profile->last_name
+                    ),
+
+
+                TextColumn::make('book.title')
+                    ->limit(20),
+
+
+                TextColumn::make('code'),
+            ])
+            ->paginated(false);
+    }
+}
--- app/Filament/Librarian/Widgets/PendingRoomBookingNotifications.php
+++ app/Filament/Librarian/Widgets/PendingRoomBookingNotifications.php
@@ -11,7 +11,7 @@ use Filament\Widgets\TableWidget as BaseWidget;
 class PendingRoomBookingNotifications extends BaseWidget
 {
 
-    protected static ?int $sort = 3;
+    protected static ?int $sort = 4;
 
     protected int | string | array $columnSpan = 2;
 
