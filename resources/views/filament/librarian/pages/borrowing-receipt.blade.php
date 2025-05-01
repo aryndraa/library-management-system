d88ba6a158ec28b9e94b3fca01ffca376c5cb26d
aryndraa <aryacoder1102@gmail.com>
2025-05-01 21:14:51 +0800

    receipt display

--- resources/views/filament/librarian/pages/borrowing-receipt.blade.php
+++ resources/views/filament/librarian/pages/borrowing-receipt.blade.php
@@ -22,7 +22,7 @@
                             class="w-[140px] h-[180px] object-cover"
                         />
                         <div class="w-full h-full ">
-                            <h2 class="text-xl font-semibold pb-2 mb-2 border-gray-300 border-b">Book Detail</h2>
+                            <h2 class="text-sm text-gray-400 font-semibold pb-2 mb-2 border-gray-300 border-b">Book Detail</h2>
 
                             <div class="mb-6">
                                 <h3 class="text-lg font-medium mb-1">{{$borrowedBook->book->title}}</h3>
@@ -30,11 +30,12 @@
                                 <p class="text-sm">{{$borrowedBook->book->isbn}}</p>
                             </div>
                             <div>
-                                <a href="" class="text-sm rounded-xl px-3 py-2 bg-primary-500 text-white font-semibold">More Detail</a>
+                                <a href="" class="text-xs rounded-xl px-3 py-2 bg-primary-500 text-white font-semibold">Book Detail</a>
                             </div>
                         </div>
                     </div>
-                    <div class="w-full">
+
+                    <div class="w-full mb-10">
                         <h2 class="font-medium text-gray-400 text-sm mb-4">Borrow Detail</h2>
                         <div class="flex flex-col gap-4  py-4 px-2 border-y">
                             <div class="grid grid-cols-4 font-medium text-gray-500   ">
@@ -52,8 +53,26 @@
                                 <span class="text-center">:</span>
                                 <p class="col-span-2 flex justify-end">{{\Carbon\Carbon::parse($borrowedBook->due_date)->translatedFormat('d M Y')}}</p>
                             </div>
-
                         </div>
+                    </div>
+
+                    <div class="flex justify-between items-center mb-2 ">
+                        <div class="flex items-center gap-3   ">
+                            @if(!$borrowedBook->member->getFirstMediaUrl('member'))
+                                <x-heroicon-s-user class="w-8 h-8 p-2 text-white bg-primary-700 rounded-full"  />
+                            @else
+                                <img src="{{$borrowedBook->member->getFirstMediaUrl('member')}}" alt="">
+                            @endif
+                            <div >
+                                <h3 class="text-sm font-medium">{{$borrowedBook->member->profile->first_name . ' ' . $borrowedBook->member->profile->last_name}}</h3>
+                                <p class="text-xs">{{$borrowedBook->member->email}}</p>
+                            </div>
+                        </div>
+
+                        <button class="text-sm text-white px-3 py-2 flex items-center gap-2 bg-primary-500 rounded-xl font-semibold">
+                            <x-heroicon-s-document-text class="w-5 h-5"/>
+                            Get Receipt
+                        </button>
                     </div>
                  @endif
             </div>
