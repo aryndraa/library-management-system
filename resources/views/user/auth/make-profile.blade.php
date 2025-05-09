d5d966bc77a5e76d2dd73588f95351b2b9143e08
aryndraa <aryacoder1102@gmail.com>
2025-05-09 08:14:50 +0800

    replace grid

--- resources/views/user/auth/make-profile.blade.php
+++ resources/views/user/auth/make-profile.blade.php
@@ -9,14 +9,15 @@
         >
             <x-logo/>
         </div>
-        <div class="mx-32 p-12 px-16 rounded-xl transform -translate-y-64 bg-white">
+        <div class="mx-5 lg:mx-32 py-4 lg:py-12 lg:px-16 rounded-xl lg:transform lg:-translate-y-64 bg-white">
             <div class="flex items-center gap-2 lg:gap-4 mb-10">
                 <hr class="w-8 lg:w-12 ">
                 <h2 class="text-sm lg:text-lg leading-1 ">Make Your Profile</h2>
             </div>
-            <form action="#" class="grid grid-cols-2 gap-8">
-                <div class="col-span-2 grid grid-cols-2 items-end gap-10 ">
-                    <div class="flex flex-col items-center ">
+
+            <form action="#" class="grid md:grid-cols-2 gap-8">
+                <div class="grid grid-rows-4">
+                    <div class="flex flex-col items-center mb-8 row-span-2">
                         <label for="avatarInput">
                             <div class="w-52 h-52 rounded-full overflow-hidden bg-bgWidget cursor-pointer">
                                 <img id="avatarPreview" src="https://via.placeholder.com/150" alt="."
@@ -25,24 +26,6 @@
                         </label>
                         <input type="file" name="avatar" id="avatarInput" accept="image/*" class="hidden">
                     </div>
-                    <div class="">
-                        <div class="flex flex-col mb-5">
-                            <label for="" class="mb-2 text-sm lg:text-base">Address</label>
-                            <input type="text" placeholder="Enter your address" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30">
-                        </div>
-                        <div class="grid grid-cols-2 gap-5">
-                            <div class="flex flex-col">
-                                <label for="" class="mb-2 text-sm lg:text-base">Province</label>
-                                <input type="text" placeholder="Enter your province" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30">
-                            </div>
-                            <div class="flex flex-col">
-                                <label for="" class="mb-2 text-sm lg:text-base">City</label>
-                                <input type="text" placeholder="Enter your city" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="grid grid-cols-2 col-span-2 gap-10 ">
                     <div class="grid grid-cols-2 gap-5">
                         <div class="flex flex-col">
                             <label for="" class="mb-2 text-sm lg:text-base">First Name</label>
@@ -52,29 +35,46 @@
                             <label for="" class="mb-2 text-sm lg:text-base">Last Name</label>
                             <input type="text" placeholder="Enter your last name" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30">
                         </div>
-                        <div class="flex flex-col col-span-full">
-                            <label for="" class="mb-2 text-sm lg:text-base">Phone Number</label>
-                            <input type="text" placeholder="Enter your phone number" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30">
+                    </div>
+                    <div class="flex flex-col col-span-full">
+                        <label for="" class="mb-2 text-sm lg:text-base">Phone Number</label>
+                        <input type="text" placeholder="Enter your phone number" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30">
+                    </div>
+                </div>
+
+                <div class="grid grid-rows-4">
+                    <div class="flex flex-col mb-5">
+                        <label for="" class="mb-2 text-sm lg:text-base">Address</label>
+                        <input type="text" placeholder="Enter your address" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30">
+                    </div>
+                    <div class="grid grid-cols-2 gap-5">
+                        <div class="flex flex-col">
+                            <label for="" class="mb-2 text-sm lg:text-base">Province</label>
+                            <input type="text" placeholder="Enter your province" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30">
+                        </div>
+                        <div class="flex flex-col">
+                            <label for="" class="mb-2 text-sm lg:text-base">City</label>
+                            <input type="text" placeholder="Enter your city" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30">
                         </div>
                     </div>
-                    <div class="flex flex-col justify-between">
-                        <div class="grid grid-cols-2 gap-5">
-                            <div class="flex flex-col">
-                                <label for="" class="mb-2 text-sm lg:text-base">Birthday</label>
-                                <input type="date" placeholder="Enter your city" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none text-font/30">
-                            </div>
-                            <div class="flex flex-col">
-                                <label for="gender" class="mb-2 text-sm lg:text-base">Gender</label>
-                                <select name="gender" id="gender"
-                                        class="w-full px-4 lg:px-6 py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none text-font/30 placeholder:text-font/30">
-                                    <option class="text-font" value="" disabled selected>Select your gender</option>
-                                    <option class="text-font" value="male">Male</option>
-                                    <option class="text-font" value="female">Female</option>
-                                    <option class="text-font" value="other">Other</option>
-                                </select>
-                            </div>
+                    <div class="grid grid-cols-2 gap-5">
+                        <div class="flex flex-col">
+                            <label for="" class="mb-2 text-sm lg:text-base">Birthday</label>
+                            <input type="date" placeholder="Enter your city" class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none text-font/30">
                         </div>
-                        <button class="flex flex-col col-span-full w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-primary-300 text-white font-medium rounded-lg focus:outline-none placeholder:text-font/30">
+                        <div class="flex flex-col">
+                            <label for="gender" class="mb-2 text-sm lg:text-base">Gender</label>
+                            <select name="gender" id="gender"
+                                    class="w-full px-4 lg:px-6 py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none text-font/30 placeholder:text-font/30">
+                                <option class="text-font" value="" disabled selected>Select your gender</option>
+                                <option class="text-font" value="male">Male</option>
+                                <option class="text-font" value="female">Female</option>
+                                <option class="text-font" value="other">Other</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="flex items-center">
+                        <button class="flex h-fit  flex-col  col-span-full w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-primary-300 text-white font-medium rounded-lg focus:outline-none placeholder:text-font/30">
                             Submit
                         </button>
                     </div>
--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,6 @@ export default defineConfig({
     ],
     server: {
         cors: true,
-        // host: true
+        host: true
     },
 });
