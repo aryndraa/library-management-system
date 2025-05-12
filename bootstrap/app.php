1e9f441d3573c577d2b23d1f00628e3d1fcbad8f
aryndraa <aryacoder1102@gmail.com>
2025-05-12 23:23:31 +0800

    middleware auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->redirectGuestsTo('member/login');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
