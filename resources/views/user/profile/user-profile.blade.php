71c321fdddb4dd0db9c8a7a7b516f3d6405e0598
aryndraa <aryacoder1102@gmail.com>
2025-05-20 10:45:35 +0800

    edit profile form

--- resources/views/user/profile/user-profile.blade.php
+++ resources/views/user/profile/user-profile.blade.php
@@ -1,30 +1,146 @@
-@php
-
-    @endphp
-
-@extends('layouts.app')
+@extends('layouts.show', ['routeDirect' => 'member.home'])
 
 @section('content')
-    <section
-        class="min-h-[70vh] w-full py-24 flex items-end  relative"
-    >
-        <div class="min-h-screen lg:max-h-[70vh] overflow-x-hidden  absolute inset-0 z-[-1]">
-{{--            <img--}}
-{{--                src="https://images.unsplash.com/photo-1646592491616-bcc3913ef52a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"--}}
-{{--                alt=""--}}
-{{--                class="lg:max-h-[70vh] z-[-2] min-w-[150%] absolute object-cover "--}}
-{{--            >--}}
-            <div
-                class="lg:max-h-[70vh] absolute z-[-1] bg-primary-300 inset-0"></div>
+    <section class="grid grid-cols-4 gap-16">
+        <div class="col-span-1 flex flex-col text-font/60  border-r">
+            <a href="#" class="pb-6 w-fit border-b  text-font border-font/40">User Profile</a>
+            <a href="#" class="py-6 w-fit border-b border-transparent">Book Borrowing</a>
+            <a href="#" class="py-6 w-fit border-b border-transparent">Book Likes</a>
+            <a href="#" class="py-6 w-fit border-b border-transparent">Room Booking</a>
+            <a href="#" class="py-6 w-fit border-b border-transparent">Account Setting</a>
         </div>
 
-        <div class="mx-20 text-white">
-            <div class="flex items-center gap-2 lg:gap-4 mb-2">
-                <hr class="w-8 lg:w-12 ">
-                <h2 class="text-sm lg:text-lg leading-1 ">Hello, {{$member->profile->first_name}} {{$member->profile->last_name}}!</h2>
+        <div class="col-span-3 flex flex-col gap-14 ">
+            <div>
+                <h1 class="text-2xl mb-10">Personal Information</h1>
+                <div class="grid grid-cols-2 gap-10">
+                    <div class="flex  col-span-2">
+                        <label for="avatarInput" class="relative">
+                            <div class="w-40 h-40 lg:w-52 lg:h-52 rounded-full overflow-hidden bg-bgWidget cursor-pointer">
+                                <img id="avatarPreview" src="{{$member->profile->photoProfile->file_url}}" alt="."
+                                     class="w-full h-full object-cover">
+
+                                <div class="absolute bottom-0 right-0 transform p-3 rounded-full bg-primary-300 -translate-x-6 -translate-y-1">
+                                    <x-heroicon-o-camera class="h-6 w-6 text-white"/>
+                                </div>
+                            </div>
+                        </label>
+                        <input type="file" name="avatar" id="avatarInput" accept="image/*" class="hidden">
+                    </div>
+                    <div class="grid grid-cols-2 col-span-2 gap-5">
+                        <div class="flex flex-col">
+                            <label for="" class="mb-2 text-sm lg:text-base">First Name</label>
+                            <input
+                                type="text"
+                                name="first_name"
+                                placeholder="Enter your last name"
+                                value="{{ $member->profile->first_name }}"
+                                class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30"
+                            >
+                        </div>
+                        <div class="flex flex-col">
+                            <label for="" class="mb-2 text-sm lg:text-base">Last Name</label>
+                            <input
+                                type="text"
+                                name="last_name"
+                                placeholder="Enter your last name"
+                                value="{{ $member->profile->last_name }}"
+                                class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30"
+                            >
+                        </div>
+                    </div>
+                    <div class="flex flex-col col-span-2">
+                        <label for="" class="mb-2 text-sm lg:text-base">Address</label>
+                        <input
+                            type="text"
+                            name="address"
+                            placeholder="Enter your address"
+                            value="{{ $member->profile->address }}"
+                            class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30"
+                        >
+                    </div>
+                    <div class="grid grid-cols-2 col-span-2 gap-5">
+                        <div class="flex flex-col">
+                            <label for="" class="mb-2 text-sm lg:text-base">Province</label>
+                            <input
+                                type="text"
+                                name="province"
+                                placeholder="Enter your province"
+                                value="{{ $member->profile->province}}"
+                                class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30"
+                            >
+                        </div>
+                        <div class="flex flex-col">
+                            <label for="" class="mb-2 text-sm lg:text-base">City</label>
+                            <input
+                                type="text"
+                                name="city"
+                                placeholder="Enter your city"
+                                value="{{ $member->profile->city }}"
+                                class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30"
+                            >
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-2 col-span-2 gap-5">
+                        <div class="flex flex-col">
+                            <label for="" class="mb-2 text-sm lg:text-base">Birthday</label>
+                            <input
+                                type="date"
+                                name="birthday"
+                                placeholder="Enter your city"
+                                value="{{ $member->profile->birthday }}"
+                                class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none text-font/30"
+                            >
+                        </div>
+                        <div class="flex flex-col">
+                            <label for="gender" class="mb-2 text-sm lg:text-base">Gender</label>
+                            <select
+                                name="gender"
+                                id="gender"
+                                class="w-full px-4 lg:px-6 py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none text-font/30 placeholder:text-font/30"
+                            >
+                                <option class="text-font" value="" disabled {{ old('gender', $member->profile->gender) ? '' : 'selected' }}>
+                                    Select your gender
+                                </option>
+                                <option class="text-font" value="male" {{ old('gender', $member->profile->gender) === 'male' ? 'selected' : '' }}>
+                                    Male
+                                </option>
+                                <option class="text-font" value="female" {{ old('gender', $member->profile->gender) === 'female' ? 'selected' : '' }}>
+                                    Female
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <h1 class="text-7xl leading-[1.2] font-medium">Account info <br>
-                & preferences.</h1>
+            <div>
+                <h2 class="text-2xl mb-10">Contact Information</h2>
+                <div class="grid grid-cols-2 gap-5">
+                    <div class="flex flex-col col-span-full">
+                        <label for="" class="mb-2 text-sm lg:text-base">Email</label>
+                        <input
+                            type="text"
+                            name="email"
+                            value="{{ $member->email }}"
+                            placeholder="Enter your email"
+                            class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30"
+                        >
+                    </div>
+                    <div class="flex flex-col col-span-full">
+                        <label for="" class="mb-2 text-sm lg:text-base">Phone Number</label>
+                        <input
+                            type="text"
+                            name="phone"
+                            value="{{ $member->profile->phone }}"
+                            placeholder="Enter your phone number"
+                            class="w-full px-4 lg:px-6  py-3 border-none focus:ring-0 bg-bgWidget rounded-lg focus:outline-none placeholder:text-font/30"
+                        >
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-end">
+                <button class="px-6 text-end py-3 w-fit rounded-lg bg-primary-300 text-white font-normal">Save</button>
+            </div>
         </div>
     </section>
 @endsection
